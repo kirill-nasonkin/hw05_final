b5a9af133a510afc17d009c488993d5653d7eacc
K_Nasonkin <sticheg2580@gmail.com>
2022-12-01 16:12:47 +0300

    Исправления после первого ревью + pep8

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -24,14 +24,24 @@ class FollowAdmin(admin.ModelAdmin):
     list_filter = ("user",)
     empty_value_display = "-пусто-"
 
+
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("pk", "post", "text", "created", "author",)
+    list_display = (
+        "pk",
+        "post",
+        "text",
+        "created",
+        "author",
+    )
     search_fields = ("text",)
-    list_filter = ("created", "author",)
+    list_filter = (
+        "created",
+        "author",
+    )
     empty_value_display = "-пусто-"
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
 admin.site.register(Follow, FollowAdmin)
 admin.site.register(Comment, CommentAdmin)
-
